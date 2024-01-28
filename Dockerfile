d70b8a6f49e510aa01834d7faae443ee4af4bfca
jsuitraining <jsuitraining@gmail.com>
2024-01-28 16:16:48 +0530

    updated repo

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM openjdk:17
-EXPOSE 8080
-ADD target/democicdpipeline.jar democicdpipeline.jar
-ENTRYPOINT ["java","-jar","/democicdpipeline.jar"]
-
-
--- src/main/java/com/practice/eventsnow/repository/EventsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.practice.eventsnow.repository;
-
-import com.practice.eventsnow.model.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventsRepository extends JpaRepository<Event,Integer> {
-}
